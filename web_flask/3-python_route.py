6629b60fec7a709dcb5b0d5d307acc1d19490298
mrktptn <marktipton95@gmail.com>
2023-11-09 22:33:18 -0600

    added route for Python with default text value (put in function argument

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""flask web app with a third route option"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text_w_spaces = text.replace('_', ' ')
+    return f'C {text_w_spaces}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    text_w_spaces = text.replace('_', ' ')
+    return f'Python {text_w_spaces}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
