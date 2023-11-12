1a1418c0aa4ec7c707a2b79b05bdaf940f6142e3
mrktptn <marktipton95@gmail.com>
2023-11-12 11:53:51 -0600

    added jinja to html file and put used the storage.all method in list_states.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """flask web app connected to file and db storage"""
 from flask import Flask, abort, render_template
-from models import storage, storage.all()
-from models import state
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def list_states():
-    return render_template(
-        'states_list.html',
-        state_id=state.id,
-        state_name=state.name
-    )
+    """returns key value pair for states"""
+    states = storage.all(State).values()
+    return render_template('states_list.html', states=states)
+
 
 
 @app.teardown_appcontext
 def teardown_context():
+    """tear down """
     storage.close()
 
 
